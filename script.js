5b31495bebecd43ace4cd49c56182fffeb3d25ca
Your name <Your email>
2024-04-16 16:17:15 +0200

    work on script

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 document.querySelector('#search').addEventListener('click', function (){
+
+//récupération du départ, de l'arrivée et de la date    
     const departureName = document.querySelector('#departure').value; 
     const arrivalName = document.querySelector('#arrival').value;
     const dateTrip = new Date(document.querySelector('#calendar').value);
-    console.log(departureName);
-    console.log(arrivalName);
-    console.log(dateTrip);
+
+//récupération de l'année du mois et du jour de date récupéré
     const newDay = dateTrip.getDate();
     const newMonth = dateTrip.getMonth()+ 1;
     const newYear = dateTrip.getFullYear();
 
-    console.log(newDay);
-    console.log(newMonth);
-    console.log(newYear);
+
     fetch(`http://localhost:3000/trips/${departureName}/${arrivalName}`)
     .then(response =>response.json())
     .then(data =>{
         const listTrip = [];
-        //const dateData = new Date(data.trip[0].date);
-        // console.log(dateData.getFullYear());
-        // console.log(dateData.getMonth()+1);
-        // console.log(dateData.getDate());
-        //console.log(data.trip[0].date.getFullYear());
         for (let i=0; i<data.trip.length; i++){
             const dateData = new Date(data.trip[i].date);
             if(dateData.getUTCFullYear()===newYear && (dateData.getUTCMonth()+1)===newMonth && dateData.getUTCDate()===newDay){
@@ -28,13 +22,14 @@ document.querySelector('#search').addEventListener('click', function (){
             }
         }
         console.log(listTrip);
-        
         for(let i=0; i < listTrip.length; i++){
-        document.querySelector('#response').innerHTML += `
-        <div class="tripsList">
-        <p>${listTrip[i].departure} > ${listTrip[i].arrival} .horaire ${listTrip[i].price}€</p>
-        <button class="book" type="button">Book</button>
-        </div>`
+            const newHoraire= new Date(listTrip[i].date);
+            document.querySelector('#response').innerHTML += `
+                <div class="tripsList">
+                <p>${listTrip[i].departure} > ${listTrip[i].arrival} ${newHoraire.getHours()}:${newHoraire.getMinutes()} ${listTrip[i].price}€</p>
+                <button class="book" type="button">Book</button>
+                </div>`
         }
+
     })
 })

8558e63f55b43022b7102a7adf9c10c2c1d1a3eb
Your name <Your email>
2024-04-17 12:48:11 +0200

    save Frontend

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 document.querySelector('#search').addEventListener('click', function (){
     
-    document.querySelector('#response').innerHTML = ``;
+    document.querySelector('#cssResponse').innerHTML = ``;
+    // fetch('http://localhost:3000/carts',{
+    //     method: 'DELETE',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({})
+
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log(data);
+    // });
+
 
 //récupération du départ, de l'arrivée et de la date    
     const departureName = document.querySelector('#departure').value; 
@@ -8,7 +19,7 @@ document.querySelector('#search').addEventListener('click', function (){
     const dateTrip = new Date(document.querySelector('#calendar').value);
 
     if(departureName==="" || arrivalName==="" || dateTrip===""){
-        document.querySelector('#response').innerHTML = `
+        document.querySelector('#cssResponse').innerHTML = `
             <div id='noTripFound'>
             <img src="images/notfound.png" alt="train">
             <p>No trip found.</p>
@@ -23,39 +34,59 @@ document.querySelector('#search').addEventListener('click', function (){
             fetch(`http://localhost:3000/trips/${departureName}/${arrivalName}`)
             .then(response =>response.json())
             .then(data =>{
+
                 const listTrip = [];
                 for (let i=0; i<data.trip.length; i++){
                     const dateData = new Date(data.trip[i].date);
                     if(dateData.getUTCFullYear()===newYear && (dateData.getUTCMonth()+1)===newMonth && dateData.getUTCDate()===newDay){
-                        listTrip.push(data.trip[i]);                      
-                        fetch('http://localhost:3000/carts', {
-                            method: 'POST',
-                            headers: { 'Content-Type': 'application/json' },
-                            body: JSON.stringify(data.trip[i])
-
-                        })
-                        .then(response => response.json())
-                        .then(data => {
-                            console.log(data);
-                        });
+                        listTrip.push(data.trip[i]);
                     }
                 }
                 console.log(listTrip);
                 if(listTrip.length===0){
-                    document.querySelector('#response').innerHTML += `
+                    document.querySelector('#cssResponse').innerHTML += `
                     <div id='noTripFound'>
                     <img src="images/notfound.png" alt="train">
                     <p>No trip found.</p>
                     </div>`;
                 }else{
+                    
                     for(let i=0; i < listTrip.length; i++){
+                        console.log(listTrip[i])
                         const newHoraire= new Date(listTrip[i].date);
-                        document.querySelector('#response').innerHTML += `
+                        document.querySelector('#cssResponse').innerHTML += `
                             <div class="tripsList">
-                            <p>${listTrip[i].departure} > ${listTrip[i].arrival} ${newHoraire.getHours()}:${newHoraire.getMinutes()} ${listTrip[i].price}€</p>
-                            <button class="book" type="button">Book</button>
+                            <p>${listTrip[i].departure} > ${listTrip[i].arrival}   ${newHoraire.getHours()}:${newHoraire.getMinutes()}   ${listTrip[i].price}€</p>
+                            <button class="book" data-id=${listTrip[i]._id} type="button">Book</button>
                             </div>`
+                        
                     }
+                }
+
+                const buttons = document.querySelectorAll('.book')
+                for(let i = 0 ; i< buttons.length ; i++) {
+                    buttons[i].addEventListener('click', function (){
+                        const tripId =this.dataset.id;
+                        //let tripToCart;
+                        console.log(tripId);
+                        fetch(`http://localhost:3000/trips/${tripId}`)
+                        .then(response =>response.json())
+                        .then(data =>{
+                            //tripToCart = data.trip;
+                            console.log(data.trip);
+                            fetch('http://localhost:3000/carts', {
+                                method: 'POST',
+                                headers: { 'Content-Type': 'application/json' },
+                                body: JSON.stringify(data.trip)
+        
+                            })
+                            .then(response => response.json())
+                            .then(data => {
+                                console.log(data);
+                            });
+
+                        })
+                    })
                 }
             })
        
